db14dfed4b045ae6c7e5f2390d8eebc5480821b4
Artek000 <artemk@softwarm.ru>
2024-06-13 22:50:31 +0300

    added Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-gray-100">
+      <body className={inter.className + " h-full"}>
+
+        <div className="min-h-full">
+          <Navbar />
+
+          <main className="-mt-32">
+            <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+              {children}
+            </div>
+          </main>
+
+        </div>
+
+      </body>
     </html>
   );
 }
